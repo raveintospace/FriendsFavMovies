6f003236041ac2f1a0b0ac80e0bc619a2d4b63b2
Oriol Angelet <uriaeri@gmail.com>
2026-01-02 09:44:11 +0100

    Initial commit

---
+++ FriendsFavMovies/FriendsFavMovies/FriendsFavMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavMoviesApp.swift
+//  FriendsFavMovies
+//
+//  Created by Uri on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
